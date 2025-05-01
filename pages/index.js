de5370358cd94806e1d2591870650a93db64aac1
Justin Turney <justinclarkturney@gmail.com>
2025-05-01 15:56:04 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -64,8 +64,6 @@ export default function Home({ githubProfileData }) {
         Skills / >
         <
         Proficiency / >
-//        <
-//        Experience / >
         <
         Projects / >
         <
